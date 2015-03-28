23b62186abfeee40df3d1af224fe85a9c8636ec8
Luke Williams <luke@iquidus.co.nz>
2015-03-29 00:48:33 +1300

    dynamic loading of data

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,32 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-function get_last_txs(cb) {
-  var uri = 'http://127.0.0.1:3001/ext/getlasttxs';
-  request({uri: uri, json: true}, function (error, response, body) {
-    return cb(body);
-  });
-}
 /* GET home page. */
 router.get('/', function(req, res) {
-  get_last_txs(function(txs) {
-    res.render('index', { title: '$JBS', txs: txs});
+  res.render('index', { title: '$JBS' });
+});
+
+router.get('/api/lasttxs', function(req, res) {
+  var uri = 'http://explorer.getjumbucks.com/ext/getlasttxs/100/100';
+  request({uri: uri, json: true}, function (error, response, body) {
+    res.json({
+      data: body,
+    });
+  });
+}); 
+
+router.get('/api/chart', function(req, res) {
+  var uri = 'http://explorer.getjumbucks.com/ext/getlasttxs/100/100';
+  request({uri: uri, json: true}, function (error, response, body) {
+    var chart = [];
+    for (var i = 0; i < body.length; i++) {
+      chart.push(body[i].total);
+      if (i == body.length - 1) {
+        res.json({
+          chart: [chart.reverse()],
+        });
+      }
+    }
   });
 });
 
