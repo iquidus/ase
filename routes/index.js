aecf7a718cb11312c06118ca2a194f2a720927da
Luke Williams <luke@iquidus.co.nz>
2015-05-07 01:43:00 +1200

    updated for 1.6.0

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,29 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var explorer = 'http://127.0.0.1:3001';
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: '$JBS' });
 });
 
 router.get('/api/lasttxs', function(req, res) {
-  var uri = 'http://explorer.getjumbucks.com/ext/getlasttxs/100/100';
+  var uri = explorer + '/ext/getlasttxs/100/100';
   request({uri: uri, json: true}, function (error, response, body) {
     res.json({
-      data: body,
+      data: body.data,
     });
   });
 }); 
 
 router.get('/api/chart', function(req, res) {
-  var uri = 'http://explorer.getjumbucks.com/ext/getlasttxs/100/100';
+  var uri = explorer + '/ext/getlasttxs/100/100';
   request({uri: uri, json: true}, function (error, response, body) {
     var chart = [];
-    for (var i = 0; i < body.length; i++) {
-      chart.push(body[i].total);
-      if (i == body.length - 1) {
+    for (var i = 0; i < body.data.length; i++) {
+      chart.push(body.data[i].total);
+      if (i == body.data.length - 1) {
         res.json({
           chart: [chart.reverse()],
         });
